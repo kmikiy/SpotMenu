8712b758c662503ac7e603d7bac2ea08d9aeb2f1
kmikiy <m.kristyan@gmail.com>
2017-09-11 21:39:27 +0200

    update preferences updated

--- SpotMenu/UpdatesPreferencesVC.swift
+++ SpotMenu/UpdatesPreferencesVC.swift
@@ -11,11 +11,51 @@ import Sparkle
 
 final class UpdatesPreferencesVC: NSViewController {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet fileprivate weak var automaticallyCheckForUpdatesButton: HoverButton!
+    @IBOutlet fileprivate weak var automaticallyDownloadUpdatesButton: HoverButton!
+    @IBOutlet fileprivate weak var lastUpdateCheckLabel: NSTextField!
+
+    // MARK: - Lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
-        
-        //SUUpdater.shared().automaticallyChecksForUpdates
-        //SUUpdater.shared().automaticallyDownloadsUpdates
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        initButtonStates()
+        lastUpdateCheckLabel.stringValue = SUUpdater.shared().lastUpdateCheckDate.asString()
+    }
+    
+    private func initButtonStates(){
+        automaticallyCheckForUpdatesButton.state = SUUpdater.shared().automaticallyChecksForUpdates.asState
+        automaticallyDownloadUpdatesButton.state = SUUpdater.shared().automaticallyDownloadsUpdates.asState
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func toggleAutomaticallyCheckForUpdates(_ sender: Any) {
+        SUUpdater.shared().automaticallyChecksForUpdates = automaticallyCheckForUpdatesButton.state.asBool
+    }
+    
+    
+    @IBAction func toggleAutomaticallyDownloadUpdates(_ sender: Any) {
+        SUUpdater.shared().automaticallyDownloadsUpdates = automaticallyDownloadUpdatesButton.state.asBool
+    }
+    
+    @IBAction func CheckForUpdates(_ sender: Any) {
+        SUUpdater.shared().checkForUpdates(sender)
+        lastUpdateCheckLabel.stringValue = SUUpdater.shared().lastUpdateCheckDate.asString()
+    }
+}
+
+extension Date {
+    func asString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm"
+        let dateString = dateFormatter.string(from:self)
+        return dateString
     }
 }
