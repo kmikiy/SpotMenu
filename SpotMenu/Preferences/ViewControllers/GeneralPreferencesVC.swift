6a1b0d477053af07e8ecb90a490b38a53bb02f0b
kmikiy <m.kristyan@gmail.com>
2017-11-04 10:57:25 +0100

    dutch and german translation updated #44

--- SpotMenu/Preferences/ViewControllers/GeneralPreferencesVC.swift
+++ SpotMenu/Preferences/ViewControllers/GeneralPreferencesVC.swift
@@ -24,7 +24,7 @@ final class GeneralPreferencesVC: NSViewController {
     @IBOutlet fileprivate weak var showSpotMenuIconButton: HoverButton!
     @IBOutlet fileprivate weak var fixPopoverToTheRightButton: HoverButton!
     @IBOutlet fileprivate weak var openAtLoginButton: HoverButton!
-    @IBOutlet weak var enableKeyboardShortcutButton: HoverButton!
+    @IBOutlet fileprivate weak var enableKeyboardShortcutButton: HoverButton!
     @IBOutlet fileprivate weak var moreInformation: NSTextField!
     @IBOutlet private weak var withLoveFromKmikiyText: NSTextField!
     
