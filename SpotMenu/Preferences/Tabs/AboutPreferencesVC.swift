ea979c7d5702aa4bdb2f51ab077c9191ba2d1e11
kmikiy <m.kristyan@gmail.com>
2017-12-03 12:44:38 +0100

    preferences refactored and minimum version bumped to 10.11

---
+++ SpotMenu/Preferences/Tabs/AboutPreferencesVC.swift
@@ -0,0 +1,18 @@
+//
+//  AboutPreferencesVC.swift
+//  SpotMenu
+//
+//  Created by Miklós Kristyán on 2017. 12. 03..
+//  Copyright © 2017. KM. All rights reserved.
+//
+
+import Cocoa
+
+class AboutPreferencesVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
