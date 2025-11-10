10f2f0da8e7de164af955063d2824ae6d1b840b8
Miklos <m.kristyan@gmail.com>
2025-11-10 08:51:39 +0000

    macos26: command , fix

--- SpotMenu/App/SpotMenuApp.swift
+++ SpotMenu/App/SpotMenuApp.swift
@@ -5,8 +5,23 @@ struct SpotMenuApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        Settings {
-            EmptyView()  // No default window
+        // Empty WindowGroup is needed for menu bar apps
+        // We handle windows manually through managers
+        WindowGroup {
+            EmptyView()
+                .frame(width: 0, height: 0)
+                .hidden()
         }
+        .commands {
+            // Replace default Settings command with our custom implementation
+            CommandGroup(replacing: .appSettings) {
+                Button("Preferences...") {
+                    appDelegate.preferencesAction()
+                }
+                .keyboardShortcut(",", modifiers: .command)
+            }
+        }
+        // Disable automatic window creation
+        .defaultSize(width: 0, height: 0)
     }
 }
